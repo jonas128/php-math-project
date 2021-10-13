7ca59302ba7181582da5c6c0f35bf7912268c959
jonas128 <jonasm@outlook.com>
2021-10-13 11:45:00 +0200

    Update index.php
    Project start

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?= 'The Elephpent in the Room'; ?>
+<?php
+$tiltle = 'web'; // string
+$decode = 1996; // integer (number)
+$hasSequel = false; // boolean
+
+// die (var_dump($decode));
+
+echo "</br>\n $decode";
+echo $tiltle;
